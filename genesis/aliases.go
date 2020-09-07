3a09f4e7664e1497cfdaaeb11764f5061e821ddc
Dan Laine <daniel.laine@avalabs.org>
2020-09-07 15:07:07 -0400

    remove spdagvm

--- genesis/aliases.go
+++ genesis/aliases.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ava-labs/gecko/vms/propertyfx"
 	"github.com/ava-labs/gecko/vms/secp256k1fx"
 	"github.com/ava-labs/gecko/vms/spchainvm"
-	"github.com/ava-labs/gecko/vms/spdagvm"
 	"github.com/ava-labs/gecko/vms/timestampvm"
 )
 
@@ -21,7 +20,6 @@ func Aliases(networkID uint32) (map[string][]string, map[[32]byte][]string, map[
 		"vm/" + platformvm.ID.String():             {"vm/platform"},
 		"vm/" + avm.ID.String():                    {"vm/avm"},
 		"vm/" + EVMID.String():                     {"vm/evm"},
-		"vm/" + spdagvm.ID.String():                {"vm/spdag"},
 		"vm/" + spchainvm.ID.String():              {"vm/spchain"},
 		"vm/" + timestampvm.ID.String():            {"vm/timestamp"},
 		"bc/" + constants.PlatformChainID.String(): {"P", "platform", "bc/P", "bc/platform"},
@@ -33,7 +31,6 @@ func Aliases(networkID uint32) (map[string][]string, map[[32]byte][]string, map[
 		platformvm.ID.Key():  {"platform"},
 		avm.ID.Key():         {"avm"},
 		EVMID.Key():          {"evm"},
-		spdagvm.ID.Key():     {"spdag"},
 		spchainvm.ID.Key():   {"spchain"},
 		timestampvm.ID.Key(): {"timestamp"},
 		secp256k1fx.ID.Key(): {"secp256k1fx"},
@@ -63,8 +60,6 @@ func Aliases(networkID uint32) (map[string][]string, map[[32]byte][]string, map[
 		case EVMID.Equals(uChain.VMID):
 			generalAliases["bc/"+chain.ID().String()] = []string{"C", "evm", "bc/C", "bc/evm"}
 			chainAliases[chain.ID().Key()] = []string{"C", "evm"}
-		case spdagvm.ID.Equals(uChain.VMID):
-			generalAliases["bc/"+chain.ID().String()] = []string{"bc/spdag"}
 			chainAliases[chain.ID().Key()] = []string{"spdag"}
 		case spchainvm.ID.Equals(uChain.VMID):
 			generalAliases["bc/"+chain.ID().String()] = []string{"bc/spchain"}
